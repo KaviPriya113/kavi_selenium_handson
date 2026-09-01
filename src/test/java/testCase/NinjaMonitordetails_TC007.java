9a8baa3843f487daed186df9f745edc9db0c6bc7
KaviPriya113 <kavipriya3311@gmail.com>
2026-09-01 21:27:02 +0530

    new testcase added

---
+++ src/test/java/testCase/NinjaMonitordetails_TC007.java
@@ -0,0 +1,24 @@
+package testCase;
+
+import org.testng.annotations.Test;
+
+import pageObjects.NinjaHomePage;
+import pageObjects.NinjaMonitorPage;
+import testBase.BaseClass;
+
+public class NinjaMonitordetails_TC007 extends BaseClass {
+	
+	@Test
+	public void monitorclass() {
+		
+		NinjaHomePage nhp = new NinjaHomePage(driver);
+		nhp.selectmonitor();
+		
+		
+		NinjaMonitorPage nmp = new NinjaMonitorPage(driver);
+		nmp.clickmonitorelement();
+		
+		
+	}
+
+}
